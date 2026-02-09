8860c597ad7661d054ff793997fc0d3318bf731d
faolanschaefer <48422672+faolanschaefer@users.noreply.github.com>
2026-02-09 16:30:30 -0500

    Implement vehicles controller

--- Assignment1_8878889/Controllers/VehiclesController.cs
+++ Assignment1_8878889/Controllers/VehiclesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VehicleInventory.Application.DTOs;
 using VehicleInventory.Application.Services;
+using VehicleInventory.Domain.Entities;
 
 namespace VehicleInventory.API.Controllers
 {
@@ -15,10 +16,42 @@ namespace VehicleInventory.API.Controllers
             _service = service;
         }
 
-        [HttpPut("{id}/status")]
-        public IActionResult UpdateStatus(int id, [FromBody] UpdateVehicleStatusDto request)
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            _service.UpdateVehicleStatus(id, request.Status);
+            var vehicles = _service.GetAllVehicles();
+            return Ok(vehicles);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var vehicle = _service.GetVehicleById(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return Ok(vehicle);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Inventory request) // TODO: Change to CreateVehicleDto
+        {
+            var createdVehicle = _service.CreateVehicle(request);
+            return CreatedAtAction(nameof(GetById), new { id = createdVehicle.Id }, createdVehicle);
+        }
+
+        [HttpPut("{id}/status")]
+        public IActionResult UpdateStatus(int id, [FromBody] string request) // TODO: Change to UpdateVehicleStatusDto
+        {
+            _service.UpdateVehicleStatus(id, request);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _service.DeleteVehicle(id);
             return NoContent();
         }
     }
