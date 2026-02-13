56ee3bf64f881a0cb37cffaa7a0983692555eaae
faolan <faolanschaefer@gmail.com>
2026-02-13 00:34:42 -0500

    Enable Swagger in production for Maintenance Web API

--- Maintenance.WebAPI/Program.cs
+++ Maintenance.WebAPI/Program.cs
@@ -12,12 +12,8 @@ builder.Services.AddScoped<IRepairHistoryService, FakeRepairHistoryService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
