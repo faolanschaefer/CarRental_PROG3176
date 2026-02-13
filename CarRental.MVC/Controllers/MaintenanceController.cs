0c08174d544a759460f61ffa3b20d39becb0400a
faolan <faolanschaefer@gmail.com>
2026-02-13 00:04:44 -0500

    Create MVC application with maintenance history

---
+++ CarRental.MVC/Controllers/MaintenanceController.cs
@@ -0,0 +1,31 @@
+﻿using CarRental.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.MVC.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MaintenanceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult History()
+        {
+            return View(new List<RepairHistoryViewModel>());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> History(int vehicleId)
+        {
+            var client = _httpClientFactory.CreateClient("MaintenanceApi");
+            var repairs = await client.GetFromJsonAsync<List<RepairHistoryViewModel>>(
+                $"api/maintenance/vehicles/{vehicleId}/repairs");
+            return View(repairs ?? new List<RepairHistoryViewModel>());
+        }
+    }
+}
