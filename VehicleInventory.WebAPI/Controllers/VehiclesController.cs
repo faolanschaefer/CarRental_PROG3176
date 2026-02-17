3c1f760633c07058448a4e4d3397b3fa99a17bd2
faolan <faolanschaefer@gmail.com>
2026-02-16 20:58:08 -0500

    Add `InvalidOperationException` catch to `UpdateStatus` inventory controller endpoint

--- VehicleInventory.WebAPI/Controllers/VehiclesController.cs
+++ VehicleInventory.WebAPI/Controllers/VehiclesController.cs
@@ -63,7 +63,7 @@ namespace VehicleInventory.API.Controllers
             {
                 return NotFound(ex.Message);
             }
-            catch (Exception ex) when (ex is ArgumentException || ex is ArgumentNullException)
+            catch (ArgumentException ex) 
             {
                 return BadRequest(ex.Message);
             }
@@ -86,6 +86,10 @@ namespace VehicleInventory.API.Controllers
                 return NotFound(ex.Message);
             }
             catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidOperationException ex)
             {
                 return BadRequest(ex.Message);
             }
