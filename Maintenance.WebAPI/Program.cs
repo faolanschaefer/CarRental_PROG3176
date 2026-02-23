416ab9388e9fb684990c549749ba9fc7bf4a1a9f
faolanschaefer <48422672+faolanschaefer@users.noreply.github.com>
2026-02-23 16:26:50 -0500

    Register API key authentication with Swagger

--- Maintenance.WebAPI/Program.cs
+++ Maintenance.WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Maintenance.WebAPI.Middleware;
 using Maintenance.WebAPI.Services;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,32 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//builder.Services.AddSwaggerGen(options =>
-//{
-//    options.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-//    {
-//        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-//        Name = "X-API-KEY",
-//        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-//        Description = "API Key authentication"
-//    });
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "X-API-KEY",
+        Type = SecuritySchemeType.ApiKey,
+        Description = "API Key authentication"
+    });
 
-//    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-//    {
-//        {
-//            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-//            {
-//                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-//                {
-//                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-//                    Id = "ApiKey"
-//                }
-//            },
-//            Array.Empty<string>()
-//        }
-//    });
-//});
+    options.AddSecurityRequirement(doc => new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecuritySchemeReference("ApiKey", doc),
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddScoped<IRepairHistoryService, FakeRepairHistoryService>();
 
